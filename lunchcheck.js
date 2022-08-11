c4daef7e56b32176a6cab993e8f137891092775c
gaurav <kgsingh81@gmail.com>
2022-08-11 11:55:02 +0530

    module1-solution

--- lunchcheck.js
+++ lunchcheck.js
@@ -1,7 +1,30 @@
-(function () {
-  'use strict';
-  angular.module('LunchCheck', [])
-  .controller('LunchCheckController', function ($scope) {
-    $scope.name1="Hello";
-  });
-}) ();
+// (function () {
+//   'use strict';
+//   angular.module('LunchCheck', [])
+//   .controller('LunchCheckController', ['$scope', function ($scope) {
+//     $scope.name1="Hello";
+//     $scope.clickbtn=function () {
+//       var count=0;
+//       var total_items=0;
+//       var txt= $scope.name1;
+//       var items=txt.split(",");
+//       count=items.length;
+//       total_items=items.length;
+//       for (var i=0;i<count;i++)
+//         {
+//           if (items[i]==" ") {
+//             total_items=total_items-1;
+//           }
+//         }
+//       console.log(total_items);
+//       if (total_items<=3) {
+//         $scope.name1="Enjoy!";
+//         }
+//       else {
+//         $scope.name1="Too Much!";
+//         }
+//     };
+//   }]);
+//   }) ();
+
+angular.module("LunchCheck",[]).controller("LunchCheckController",["$scope",function(a){a.name1="Hello",a.clickbtn=function(){var e=0,b=0,c=a.name1.split(",");e=c.length,b=c.length;for(var d=0;d<e;d++)" "==c[d]&&(b-=1);console.log(b),b<=3?a.name1="Enjoy!":a.name1="Too Much!"}}])
